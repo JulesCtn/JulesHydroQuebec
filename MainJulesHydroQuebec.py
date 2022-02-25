f6f4064ff6c5f711d45d8b9b6524c5fe2b0241f9
IamFlucs <louis.contadin@outlook.fr>
2022-02-25 23:10:19 +0100

    Update MainJulesHydroQuebec.py

--- MainJulesHydroQuebec.py
+++ MainJulesHydroQuebec.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from PIL import ImageTk,Image
 
+master = Tk()
 #Settings of the window
-tab = Tk()
-tab.title("Test semaine 2")
-tab.geometry('600x400')
+master.title("System Production")
+master.geometry('1280x720')
+master.minsize(1280, 720)
+master.iconbitmap("logo.ico")
 
 #fonction click
 def myClick():
@@ -12,18 +14,18 @@ def myClick():
     label2.configure(text=result)
 
 #Creating Label Widget
-label1 = Label(tab, text="Entrez une valeur")
-label2 = Label(tab, height=2, width=10)
-entry1 = Entry(tab, width=10, borderwidth=3)
-Buton1 = Button(tab, text="Display entry", command=myClick)
+label1 = Label(master, text="Entrez une valeur")
+label2 = Label(master, height=2, width=10)
+entry1 = Entry(master, width=10, borderwidth=3)
+Buton1 = Button(master, text="Display entry", command=myClick)
 
 #Creating Δ for Centrale (100x87)
 Centrale = PhotoImage(file="Centrale.png")
-canvas1 = Canvas(tab,width=100, height=87)
+canvas1 = Canvas(master,width=100, height=87)
 canvas1.create_image(0, 0, anchor=NW, image=Centrale)
 #Creating ▭ for Reservoir (100x100)
 Reservoir = PhotoImage(file="Reservoir.png")
-canvas2 = Canvas(tab,width=100, height=100)
+canvas2 = Canvas(master,width=100, height=100)
 canvas2.create_image(0, 0, anchor=NW, image=Reservoir)
 
 
@@ -36,4 +38,4 @@ label2.grid(row=4, column=2, padx=10)
 canvas1.grid(row=5, column=2)
 canvas2.grid(row=6, column=2)
 
-tab.mainloop()
+master.mainloop()
