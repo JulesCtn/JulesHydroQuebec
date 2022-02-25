47238c5a97c41b0f28433080dbcf81375ea348c5
JulesCtn <jules.contadin@laposte.net>
2022-02-24 19:49:56 -0500

    All The Code
    - Δ for Centrale (100x87)
    - ▭ for Reservoir (100x100)
    - Main for Testing

--- MainJulesHydroQuebec.py
+++ MainJulesHydroQuebec.py
@@ -4,7 +4,7 @@ from PIL import ImageTk,Image
 #Settings of the window
 tab = Tk()
 tab.title("Test semaine 2")
-tab.geometry('400x200')
+tab.geometry('600x400')
 
 #fonction click
 def myClick():
@@ -15,19 +15,25 @@ def myClick():
 label1 = Label(tab, text="Entrez une valeur")
 label2 = Label(tab, height=2, width=10)
 entry1 = Entry(tab, width=10, borderwidth=3)
-myButon = Button(tab, text="Display entry", command=myClick)
+Buton1 = Button(tab, text="Display entry", command=myClick)
 
-#Creating Δ for Centrale
-photo = PhotoImage(file="Centrale.png")
-canvas = Canvas(tab,width=350, height=200)
-canvas.create_image(0, 0, anchor=NW, image=photo)
+#Creating Δ for Centrale (100x87)
+Centrale = PhotoImage(file="Centrale.png")
+canvas1 = Canvas(tab,width=100, height=87)
+canvas1.create_image(0, 0, anchor=NW, image=Centrale)
+#Creating ▭ for Reservoir (100x100)
+Reservoir = PhotoImage(file="Reservoir.png")
+canvas2 = Canvas(tab,width=100, height=100)
+canvas2.create_image(0, 0, anchor=NW, image=Reservoir)
+
 
 #Showing it onto the screen
-label1.grid(row=0, column=0, padx=10)
-label2.grid(row=1, column=0, padx=10)
-entry1.grid(row=0, column=1, padx=10)
-myButon.grid(row=0, column=2)
+label1.grid(row=1, column=2, padx=10,pady=5)
+entry1.grid(row=2, column=2, padx=10,pady=5)
+Buton1.grid(row=3, column=2)
+label2.grid(row=4, column=2, padx=10)
 
-canvas.grid(row=2, column=1)
+canvas1.grid(row=5, column=2)
+canvas2.grid(row=6, column=2)
 
 tab.mainloop()
