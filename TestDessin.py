3b9ceea8022c99b8ff940f3f858d1e311f10f41e
JulesCtn <jules.contadin@laposte.net>
2022-03-23 16:44:51 -0400

    Create TestDessin.py

---
+++ TestDessin.py
@@ -0,0 +1,68 @@
+from tkinter import *
+from turtle import color
+from PIL import ImageTk,Image
+
+master = Tk()
+#Settings of the main window
+master.title("System Production")
+master.geometry('800x600')
+master.minsize(800, 600)
+master.iconbitmap("logo.ico")
+
+#Fonction click pour | + Δ + ▭ + |   
+def myClick():
+    Delta=0
+    canvas.create_line(Delta,50,Delta+25,50,fill="black",width=2) # |
+    Delta=Delta+25
+    canvas.create_rectangle(Delta,25,Delta+25,75,fill="royalblue", outline="black",width=2) # ▭ reservoir (x1, y1, x2, y2)
+    Delta=Delta+25
+    canvas.create_polygon(Delta,25,Delta,75,Delta+25,50,fill="royalblue",outline="black", width=2)# Δ Centrale (x1, y1, x2, y2, x3, y3)
+    Delta=Delta+25
+    canvas.create_line(Delta,50,Delta+25,50,fill="black",width=2) # |
+    Delta=Delta+25
+
+#Fonction click pour | + Δ + |
+def myCentrale():
+    Delta=0
+    canvas.create_line(Delta,50,Delta+25,50,fill="black",width=2) # |
+    Delta=Delta+25
+    canvas.create_polygon(Delta,25,Delta,75,Delta+50,50,fill="royalblue",outline="black", width=2)# Δ Centrale (x1, y1, x2, y2, x3, y3)
+    Delta=Delta+50
+    canvas.create_line(Delta,50,Delta+25,50,fill="black",width=2) # |
+    Delta=Delta+25
+
+#Fonction click pour | + ▭ + |
+def myReservoir():
+    Delta=0
+    canvas.create_line(Delta,50,Delta+25,50,fill="black",width=2) # |
+    Delta=Delta+25
+    canvas.create_rectangle(Delta,25,Delta+50,75,fill="royalblue", outline="black",width=2) # ▭ reservoir (x1, y1, x2, y2)
+    Delta=Delta+50
+    canvas.create_line(Delta,50,Delta+25,50,fill="black",width=2) # |
+    Delta=Delta+25
+    
+#Fonction click pour clear dessin (dessin d'un grand carré de la même couleur que le fond)
+def myClear():
+    Delta=0
+    canvas.create_rectangle(Delta+4,Delta+4,Delta+100,100,fill="grey94",outline="grey94")
+    
+
+#Creation du bouton pour Δ + ▭
+Buton1 = Button(master, text="Add Centrale + Reservoir", command=myClick)
+Buton2 = Button(master, text="Add Centrale", command=myCentrale)
+Buton3 = Button(master, text="Add Reservoir", command=myReservoir)
+Buton4 = Button(master, text="Clear", command=myClear)
+
+#Canvas creation des dessins
+canvas = Canvas(master, height=100, width=500)
+canvas.create_line(3,3,3,100,fill="black",width=2)
+#canvas.create_rectangle(0, 0, 600, 600, fill="grey") # le fond pour mieu comprendre le canvas (x1, y1, x2, y2)
+
+#Showing it onto the screen
+Buton1.grid(row=1, column=1)
+Buton2.grid(row=2, column=1)
+Buton3.grid(row=3, column=1)
+Buton4.grid(row=4, column=1)
+canvas.grid(row=2 , column=2) # canva dessin
+
+master.mainloop()
